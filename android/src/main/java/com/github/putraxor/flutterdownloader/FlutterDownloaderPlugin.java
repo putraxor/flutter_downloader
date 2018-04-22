d9b572aa597fe4e883a39446445db776329224cd
Ardiansyah Putra <putraxor@gmail.com>
2018-04-23 03:59:42 +0700

    fix request permission flow

--- android/src/main/java/com/github/putraxor/flutterdownloader/FlutterDownloaderPlugin.java
+++ android/src/main/java/com/github/putraxor/flutterdownloader/FlutterDownloaderPlugin.java
@@ -3,14 +3,17 @@ package com.github.putraxor.flutterdownloader;
 import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.DownloadManager;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 import static android.content.Context.DOWNLOAD_SERVICE;
@@ -19,22 +22,49 @@ import static android.content.Context.DOWNLOAD_SERVICE;
  * FlutterDownloaderPlugin
  */
 public class FlutterDownloaderPlugin implements MethodCallHandler {
-    static DownloadManager dm;
-    static private String writePerm = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+    private DownloadManager dm;
+    private final String WRITE_PERM = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+    private final int WRITE_PERM_REQ = 12321;
+    private Registrar registrar;
+    private String uri, fileName;
+
+    /**
+     * Constructor
+     *
+     * @param registrar
+     */
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    private FlutterDownloaderPlugin(Registrar registrar) {
+        this.dm = (DownloadManager) registrar.activeContext().getSystemService(DOWNLOAD_SERVICE);
+        this.registrar = registrar;
+        registrar.addRequestPermissionsResultListener(new PluginRegistry.RequestPermissionsResultListener() {
+            @Override
+            public boolean onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+                boolean granted = grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                if (requestCode == WRITE_PERM_REQ && granted) {
+                    executeDownload();
+                }
+                return granted;
+            }
+        });
+    }
+
 
     /**
      * Plugin registration.
      */
-    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+
     public static void registerWith(Registrar registrar) {
-        dm = (DownloadManager) registrar.context().getSystemService(DOWNLOAD_SERVICE);
         final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_downloader");
-        channel.setMethodCallHandler(new FlutterDownloaderPlugin());
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            registrar.activity().requestPermissions(new String[]{writePerm}, 11111);
-        }
+        channel.setMethodCallHandler(new FlutterDownloaderPlugin(registrar));
     }
 
+    /**
+     * On method call
+     *
+     * @param call
+     * @param result
+     */
     @Override
     public void onMethodCall(MethodCall call, Result result) {
         if (call.method.equals("download")) {
@@ -44,20 +74,41 @@ public class FlutterDownloaderPlugin implements MethodCallHandler {
         }
     }
 
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    /**
+     * Create native downloader
+     *
+     * @param call
+     * @param result
+     */
     private void download(MethodCall call, Result result) {
-        try {
-            String uri = call.argument("url");
-            String fileName = call.argument("fileName");
-            DownloadManager.Request r = new DownloadManager.Request(Uri.parse(uri));
-            r.setVisibleInDownloadsUi(true);
-            r.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
-            r.allowScanningByMediaScanner();
-            r.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
-            dm.enqueue(r);
-            result.success("Download started");
-        } catch (Exception e) {
-            result.error("Download failed", e.getMessage(), e);
+        uri = call.argument("url");
+        fileName = call.argument("fileName");
+        boolean granted = ActivityCompat.checkSelfPermission(registrar.activity(), WRITE_PERM) == PackageManager.PERMISSION_GRANTED;
+        if (granted) {
+            try {
+                executeDownload();
+                result.success("Download started");
+            } catch (Exception e) {
+                result.error("Download failed", e.getMessage(), e);
+            }
+        } else {
+            ActivityCompat.requestPermissions(registrar.activity(), new String[]{WRITE_PERM}, WRITE_PERM_REQ);
         }
+
+    }
+
+    /**
+     * Execute download manager
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void executeDownload() {
+        DownloadManager.Request r = new DownloadManager.Request(Uri.parse(uri));
+        r.setTitle("Download");
+        r.setDescription("Processing download request...");
+        r.setVisibleInDownloadsUi(true);
+        r.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
+        r.allowScanningByMediaScanner();
+        r.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
+        dm.enqueue(r);
     }
 }
